9484c2af835f8a34e3780482775abed4e381250e
Mikee Rmz <mikee.rmz@gmail.com>
2018-05-09 13:21:41 -0500

    Addin children to create body content
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,64 +1,57 @@
+// Dependencies
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+// Styles
 import './App.css';
+
+// Components
 import Inicio from './components/Landing Page/Inicio';
+import Content from './components/Global/Content';
 import Nvar from './components/Nvar/Nvar';
 import Registro from './components/Registro/Registro';
 import Login from './components/login/Login';
-import GetReservUsuario from './components/reservacionesXusuario/getReservUsuario';
-import GetPropLanding from './components/propiedadesLanding/GetPropLanding';
-import Evaluacion from './components/evaluacion/evaluacion';
-import CreateProperty from './components/CreateProperty/CreateProperty';
-import Profile from './components/Profile/Profile';
-import Reservation from './components/Reservation/Reservation';
+
 
 class App extends Component {
-  static contextTypes = {
-    authenticated: localStorage.getItem('token')
-  }
-
   constructor(props){
     super(props);
     this.state = {
       authenticated: localStorage.getItem('token')
     }
   }
+
+  static propTypes = {
+    children: PropTypes.object.isRequired
+  }
+
   render() {
+    const { children } = this.props
 
     if (!this.state.authenticated) {
       return (
-
         <div className="App Seccion">
           <Nvar />
           <div className="container">
             <div className="p-5">
               <Inicio/>
             </div>
-            
               <Registro/>
               <Login/>
-              
           </div>
         </div>
       );
     } else {
       return (
-
         <div className="App Seccion">
-          <Nvar />
+          <Nvar/>
           <div className="container">
-            <div className="p-5">
-            </div>
-              <Reservation/>
-              <Profile/>
+            <div className="p-5"> </div>
+              <Content body={children}/>
           </div>
-          <Registro/>
-          <Login/>
-          <GetReservUsuario/>
-          <GetPropLanding/>
-          <Evaluacion/>
         </div>
       )
-      
+
     }
   }
 }
