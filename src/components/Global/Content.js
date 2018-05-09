e8ef722af98e66a7f7d9fea2f7b253a122cc44b5
Mikee Rmz <mikee.rmz@gmail.com>
2018-05-09 13:22:14 -0500

    Add content js to charge body
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

---
+++ src/components/Global/Content.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Content extends Component {
+	static propTypes = {
+		body: PropTypes.object.isRequired
+  }
+	render() {
+		const { body } = this.props;
+		console.log( body)
+		return(
+			<div className="Content">
+				{body}
+			</div>
+		)
+	}
+}
+
+export default Content;
