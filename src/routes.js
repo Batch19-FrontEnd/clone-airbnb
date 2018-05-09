14d75f8d22c01adbe35a305830a037f9b4a0cc3d
Mikee Rmz <mikee.rmz@gmail.com>
2018-05-09 13:21:52 -0500

    Add new routes to app
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- src/routes.js
+++ src/routes.js
@@ -4,21 +4,25 @@ import {Route, Switch} from 'react-router-dom';
 
 /*Componentes*/
 import App from './App';
-import Inicio from './components/Landing Page/Inicio';
-import Nvar from './components/Nvar/Nvar';
-import Registro from './components/Registro/Registro';
-import Login from './components/login/Login';
 import Profile from './components/Profile/Profile';
 import Reservation from './components/Reservation/Reservation';
+import GetReservUsuario from './components/reservacionesXusuario/getReservUsuario';
+import GetPropLanding from './components/propiedadesLanding/GetPropLanding';
+import Evaluacion from './components/evaluacion/evaluacion';
+import CreateProperty from './components/CreateProperty/CreateProperty';
+import Page404 from './components/Page404/Page404';
 
-const AppRoutes = () => 
+
+const AppRoutes = () =>
     <App>
         <Switch>
-            <Route path="/" component= {Inicio} />
-            <Route path="/Registro" component={Registro}/>
-            <Route path="/Login" component={Login}/>
             <Route path="/Profile" component={Profile}/>
             <Route path="/Reservation" component={Reservation}/>
+            <Route path="/GetReservUsuario" component={GetReservUsuario}/>
+            <Route path="/GetPropLanding" component={GetPropLanding}/>
+            <Route path="/Evaluacion" component={Evaluacion}/>
+            <Route path="/CreateProperty" component={CreateProperty}/>
+            <Route component={Page404}/>
         </Switch>
     </App>
 
