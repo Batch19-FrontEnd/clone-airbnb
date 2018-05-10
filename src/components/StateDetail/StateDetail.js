ec78dea1c169e52e81ebede2f313638a9e4cacfd
unknown <hmae.hector@gmail.com>
2018-05-10 10:53:12 -0500

    se agrego react-icons
    Signed-off-by: unknown <hmae.hector@gmail.com>

--- src/components/StateDetail/StateDetail.js
+++ src/components/StateDetail/StateDetail.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import './stateDetail.css';
 import { Col, Button, Form, FormGroup, Label, Input, FormText, Container, Row } from 'reactstrap';
 import axios from 'axios';
-
+import FaIconPack from 'react-icons/lib/fa'
+import FaBed from 'react-icons/lib/fa/bed';
 //import para  traer el componenet raiz de react
 
 //Se crea clase con  mismo nombre que componente
@@ -23,7 +24,7 @@ class StateDetail extends Component {
     axios.get('https://airbnb-cn-b19.herokuapp.com/api/v1/estates/7')
       .then(Response => {
         let detail = Response.data;
-        console.log('detail:', detail);
+        console.log('detail:', detail.Service.beds);
 
         this.setState({ detail });
       });
@@ -60,6 +61,13 @@ class StateDetail extends Component {
               )}
             </Col>
           </Row>
+          <hr/>
+          <Row>
+          <Col>
+          <h3><FaBed /></h3>
+          </Col>
+          </Row>
+
         </Container>
       </div>
     );
