adf58b352b3ce07b4e57719253ece99facb0e358
LigorioSalgado <salgadolg5@hotmail.com>
2018-05-11 18:08:19 -0500

    cambios para poner propiedades en landing

--- src/components/propiedadesLanding/PropiedadesLanding.js
+++ src/components/propiedadesLanding/PropiedadesLanding.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import {Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle,Col,Row } from 'reactstrap';
 import './PropiedadesLanding.css';
 import StarRatingComponent from 'react-star-rating-component';
+
+
 
 class PropiedadesLanding extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            name: props.nombre === null ? "Nombre no disponible" : props.nombre,
-            pais: props.pais === null ? "Pais no disponible" : props.pais,
-            ciudad:props.ciudad === null ? "Ciudad no disponible" : props.ciudad,
-            foto: props.foto === null ? "http://zazsupercentro.com/wp-content/uploads/2017/07/imagen-no-disponible.png" : props.foto,
-            precio: props.precio === null ? "Precio no disponible" : props.precio,
-            rating: props.rating === null ? 0 : props.rating
+            propiedades:[]
         };
 
     }
 
+    componentWillMount(){
+        Axios.get("http://airbnb-cn-b19.herokuapp.com/api/v1/estates/view").then((resp) =>{
+            this.setState({propiedades:resp.data})
+        }).catch(() => {
+        })
 
-    render() {
-        return (
-                <div>
-                    <Card className="col-3">
-                        <CardImg top width="100%" src={this.state.foto} alt="Card image cap" />
+    }
+
+
+    buildCards(){
+        return this.state.propiedades.map(
+            (propiedad) =>(
+                    <Col md="3">
+                    <Card style={{marginTop:"10px"}} >
+                        <CardImg top width="100%" height="200" src={propiedad.photos !== null ? propiedad.photos[0] : "" 
+                         } alt="Card image cap" />
                         <CardBody>
                             <div className="tituloCiudad">
-                                <CardSubtitle>{this.state.ciudad}</CardSubtitle>
+                                <CardSubtitle>{propiedad.Address.estado}</CardSubtitle>
                                 <CardSubtitle>·</CardSubtitle>
-                                <CardSubtitle>{this.state.pais}</CardSubtitle>                              
+                                <CardSubtitle>{propiedad.Address.pais}</CardSubtitle>                              
                             </div>
-                            <CardTitle>{this.state.name}</CardTitle>
-                            <CardText>${this.state.precio} por noche</CardText>
+                            <CardTitle>{propiedad.estate_name}</CardTitle>
+                            <CardText>${propiedad.price} por noche</CardText>
                             <div>
                                 <StarRatingComponent 
                                 name="rate2" 
@@ -41,7 +48,18 @@ class PropiedadesLanding extends Component {
                             </div>
                         </CardBody>
                     </Card>
-                </div>
+                    </Col>
+            )
+        )
+    }
+
+
+    render() {
+        return (
+            <Row>
+
+                {this.buildCards()}
+            </Row>
         )
     }
 
