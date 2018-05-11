2d2a6981ba75801d313aaa1a39789036acec2ba8
Mikee Rmz <mikee.rmz@gmail.com>
2018-05-11 16:18:19 -0500

    Add modal to login
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- src/components/Landing Page/Inicio.js
+++ src/components/Landing Page/Inicio.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import './Estilos.css';
 import { Jumbotron, Button, Row, Col, Input, InputGroup, InputGroupAddon, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Registro from '../../components/Registro/Registro';
+import Login from '../../components/login/Login';
+
 
 class Inicio extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            modalRegistro: false
+            modalRegistro: false,
+            modalLogin: false
         };
 
         this.toggle = this.toggle.bind(this);
+        this.toggleLogin = this.toggleLogin.bind(this);
     }
 
     toggle() {
@@ -20,12 +24,10 @@ class Inicio extends Component {
         });
     }
 
-    componentWillMount() {
-        //Aqui se hacen peticiones a una API
-    }
-
-    componentWillReceiveProps() {
-        //Si el componente padre pasa props despues de renderear
+    toggleLogin() {
+        this.setState({
+            modalLogin: !this.state.modalLogin
+        });
     }
 
     render() {
@@ -48,10 +50,13 @@ class Inicio extends Component {
 
                     <Row>
                         <Col></Col>
-                        <Col><Button href="Login" color="success">Acceder</Button></Col>
+                        <Col><Button onClick={this.toggleLogin} color="success">Acceder</Button></Col>
                         <Col><Button onClick={this.toggle} color="primary">Registrarse</Button></Col>
                         <Modal isOpen={this.state.modalRegistro} toggle={this.toggle} className={this.props.className}>
                             <Registro/>
+                        </Modal>
+                        <Modal isOpen={this.state.modalLogin} toggle={this.toggleLogin} className={this.props.className}>
+                            <Login/>
                         </Modal>
                         <Col></Col>
                     </Row>
